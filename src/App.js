88a9211655c2e22b9441444685036bd1c6e79f8d
Thorben <bender.thorben0@gmail.com>
2020-11-25 16:16:46 +0100

    Add route to redirect to leaguesview

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Component } from 'react';
 import { Route } from 'react-router-dom';
 import LeagueView from './Components/LeagueView';
+import { Redirect } from 'react-router-dom';
 
 class App extends Component {
   render() {
-    return <Route exact path="/league/:id" component={LeagueView} />;
+    return (
+      <div>
+        <Route exact path="/" render={() => <Redirect to="leagues" />} />
+        <Route exact path="/league/:id" component={LeagueView} />
+      </div>
+    );
   }
 }
 
